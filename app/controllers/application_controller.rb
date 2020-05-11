4216ccd836cb78a573023accaab30f0d033d006e
lgm527 <laurellmccaffrey@gmail.com>
2020-05-11 13:58:04 -0400

    get requests made for every curl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
+require 'net/http'
+require 'json'
+
 class ApplicationController < ActionController::Base
   def index
-    render text: "Thanks for sending a GET request with cURL! Payload: #{request.body.read}"
+    urls = ['https://takehome.io/twitter', 'https://takehome.io/facebook', 'https://takehome.io/instagram']
+    @result = Array.new
+    urls.each do |url|
+      escaped_address = URI.escape(url)
+      uri = URI.parse(escaped_address)
+      res = Net::HTTP.get_response(uri)
+      if res.is_a?(Net::HTTPSuccess)
+        @result.push(JSON.parse(res.body))
+      else
+        @result.push(res.body)
+      end
+    end
+    @result = @result.flatten
   end
+
 end
