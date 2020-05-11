4d471ad59e6a347d4b69b65bc8efe19ce8a0dc28
lgm527 <laurellmccaffrey@gmail.com>
2020-05-11 18:07:38 -0400

    multiple jsons rendered

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,18 +4,28 @@ require 'json'
 class ApplicationController < ActionController::Base
   def index
     urls = ['https://takehome.io/twitter', 'https://takehome.io/facebook', 'https://takehome.io/instagram']
-    @result = Array.new
-    urls.each do |url|
+    urls.each_with_index do |url, i|
       escaped_address = URI.escape(url)
       uri = URI.parse(escaped_address)
-      res = Net::HTTP.get_response(uri)
-      if res.is_a?(Net::HTTPSuccess)
-        @result.push(JSON.parse(res.body))
-      else
-        @result.push(res.body)
+
+      if i == 0
+        res = Net::HTTP.get_response(uri)
+        @twitter = res.body === "I am trapped in a social media factory send help" ? res.body : JSON.parse(res.body)
+      elsif i == 1
+        res1 = Net::HTTP.get_response(uri)
+        @facebook = res1.body === "I am trapped in a social media factory send help" ? res1.body : JSON.parse(res1.body)
+      elsif i == 2
+        res2 = Net::HTTP.get_response(uri)
+        @instagram = res2.body === "I am trapped in a social media factory send help" ? res2.body : JSON.parse(res2.body)
       end
+
     end
-    @result = @result.flatten
+    @content = {
+      twitter: @twitter,
+      facebook: @facebook,
+      instagram: @instagram
+    }
+    render plain: @content.to_json
   end
 
 end
